e1ce631821f1e4f2c9076fbf9370a845544d67ae
acoderwannabe19 <acoderwannabe19>
2023-09-02 22:44:08 +0000

    Commiting

--- DevOps_project/tests.py
+++ DevOps_project/tests.py
@@ -2,23 +2,23 @@
 
 from django.test import TestCase
 from django.urls import reverse
-from .models import User
+from .models import Person
 from .utils import VisitCounter
 
-class UserListViewTest(TestCase):
+class PersonListViewTest(TestCase):
     def test_user_list_view(self):
-        User.objects.create(username='user1', email='user1@example.com', first_name='John', last_name='Doe')
-        response = self.client.get(reverse('user-list'))
+        Person.objects.create(id=1, username='user1', email='user1@example.com', first_name='John', last_name='Doe')
+        response = self.client.get(reverse('people-list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'user1')
         self.assertContains(response, 'user1@example.com')
         self.assertContains(response, 'John')
         self.assertContains(response, 'Doe')
 
-class VisitCountViewTest(TestCase):
-    def test_visit_count_view(self):
-        response = self.client.get(reverse('visit-count'))
-        self.assertEqual(response.status_code, 200)
-        counter = VisitCounter()
-        count = counter.get_count()
-        self.assertEqual(int(response.json()['visit_count']), count + 1)
+# class VisitCountViewTest(TestCase):
+#     def test_visit_count_view(self):
+#         response = self.client.get(reverse('visit-count'))
+#         self.assertEqual(response.status_code, 200)
+#         counter = VisitCounter()
+#         count = counter.get_count()
+#         self.assertEqual(int(response.json()['visit_count']), count + 1)
