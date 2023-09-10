85ad6cc832195de6a02926e9c5ca097045ab5a26
acoderwannabe19 <acoderwannabe19>
2023-09-10 13:48:45 +0000

    Tested :)

--- DevOps_project/settings.py
+++ DevOps_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,7 +88,7 @@ DATABASES = {
         "NAME": "people",
         "USER": "postgres",
         "PASSWORD": "awa",
-        "HOST": "db",
+        "HOST": os.getenv("DJANGO_DB_HOST", "localhost"),
         "PORT": "5432",
     }
 }
