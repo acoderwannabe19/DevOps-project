9108d36a57f21939cf492476fb58f5df947bf932
acoderwannabe19 <acoderwannabe19>
2023-09-11 08:49:39 +0000

    Endpoints tested

--- DevOps_project/settings.py
+++ DevOps_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,9 +85,8 @@ DATABASES = {
     }
 }
 # Redis settings
-REDIS_HOST = 'redis'
+REDIS_HOST = "redis"
 REDIS_PORT = 6379
-
 
 
 # Password validation
--- DevOps_project/tests.py
+++ DevOps_project/tests.py
@@ -3,20 +3,28 @@ from django.urls import reverse
 from .models import Person
 from .utils import VisitCounter
 
+
 class PersonListViewTest(TestCase):
     def test_user_list_view(self):
-        Person.objects.create(id=1, username='user1', email='user1@example.com', first_name='John', last_name='Doe')
-        response = self.client.get(reverse('people-list'))
+        Person.objects.create(
+            id=1,
+            username="user1",
+            email="user1@example.com",
+            first_name="John",
+            last_name="Doe",
+        )
+        response = self.client.get(reverse("people-list"))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'user1')
-        self.assertContains(response, 'user1@example.com')
-        self.assertContains(response, 'John')
-        self.assertContains(response, 'Doe')
+        self.assertContains(response, "user1")
+        self.assertContains(response, "user1@example.com")
+        self.assertContains(response, "John")
+        self.assertContains(response, "Doe")
+
 
 class VisitCountViewTest(TestCase):
     def test_visit_count_view(self):
-        response = self.client.get(reverse('visit-count'))
+        response = self.client.get(reverse("visit-count"))
         self.assertEqual(response.status_code, 200)
         counter = VisitCounter()
         count = counter.get_count()
-        self.assertEqual(int(response.json()['visit_count']), count)
+        self.assertEqual(int(response.json()["visit_count"]), count)
