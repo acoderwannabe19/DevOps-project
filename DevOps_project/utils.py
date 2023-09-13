adb2c11c11867bab2c328d628e7da268fb26615a
acoderwannabe19 <acoderwannabe19>
2023-09-13 15:42:42 +0000

    Testing

--- DevOps_project/utils.py
+++ DevOps_project/utils.py
@@ -4,9 +4,7 @@ import os
 
 class VisitCounter:
     def __init__(self):
-        self.redis_client = redis.StrictRedis(
-            host="redis", port=6379, db=0
-        )
+        self.redis_client = redis.StrictRedis(host="redis", port=6379, db=0)
 
     def increment(self):
         self.redis_client.incr("visit_count")
