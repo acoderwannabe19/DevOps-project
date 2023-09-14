c8151fa10830d089f4e8cff22a5392030cc3447d
acoderwannabe19 <acoderwannabe19>
2023-09-14 07:29:53 +0000

    Testing

--- DevOps_project/settings.py
+++ DevOps_project/settings.py
@@ -92,7 +92,7 @@ CACHES = {
         "LOCATION": f'redis://{os.environ.get("REDIS_HOST", "redis")}:6379/1',
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
     }
 }
 
