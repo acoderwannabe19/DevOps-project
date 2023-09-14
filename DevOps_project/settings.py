bb8ebf563f34ef48192adc93f3b8ed00ddc29564
Bintou <ngbtening@ept.sn>
2023-09-14 12:09:47 +0000

    Test

--- DevOps_project/settings.py
+++ DevOps_project/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'people',
-        'USER': os.getenv('DJANGO_DB_USER'),
-        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
-        'HOST': os.getenv('DJANGO_DB_HOST'),
+        'USER': os.getenv('DJANGO_DB_USER','postgres'),
+        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD','awa'),
+        'HOST': os.getenv('DJANGO_DB_HOST','db'),
         'PORT': os.getenv('DJANGO_DB_PORT', 5432)
     }
 }
